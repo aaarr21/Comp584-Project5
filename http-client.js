33bfc9efdfd64106991fdce179a0f0852108d8ab
aaarr21 <john102111@gmail.com>
2025-12-18 23:15:02 -0800

    final

--- http-client.js
+++ http-client.js
@@ -1,8 +1,9 @@
-
+// load the http module
 const http = require('http');
-
+// get the URL from command line arguments
 const url = process.argv[2];
 
+// make an HTTP GET request to the URL
 http.get(url, function (response) {
   // Interpret response bytes as UTF-8 strings
   response.setEncoding('utf8');
--- my-first-async-io.js
+++ my-first-async-io.js
@@ -2,6 +2,7 @@
 // load the fs module
 const fs = require('fs');
 
+// read the file asynchronously
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
     if (err) {
         return console.error(err);
